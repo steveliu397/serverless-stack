cb0ca4ef8fd7b03e347cf032c6522ef1261b6e06
Steve Liu <steveliu397@Steves-MacBook-Air.local>
2023-02-24 16:24:39 -0800

    updated main

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,18 @@
 package main
 
 import{
-	
+	"os"
+	"github.com/aws/aws-skd-go/aws/session"
 }
 
 func main(){
-	
+	region := os.Getenv("AWS_REGION")
+	awsSession, err := session.NewSession(&aws.Config{}})
+}
+
+const tableName = "LambdaInGoUser"
+
+func handler(req events.APIGatewayProxyRequest) (*events.APIGatewayProxyRequest, error){
+		switch req 
 }
 
